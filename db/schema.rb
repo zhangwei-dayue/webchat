b65ac88d9e34482586d2905f9fecc0659b1c6de9
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-04 06:27:08 +0800

    增加friendship的model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703220546) do
+ActiveRecord::Schema.define(version: 20170703222520) do
 
   create_table "friends", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.boolean  "confirmed",  default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
