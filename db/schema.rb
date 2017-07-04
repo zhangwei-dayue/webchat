95439ec948c2470eef1ab5cdc3f6ba4be6d3af0d
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-05 05:44:08 +0800

    增加message的model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704211629) do
+ActiveRecord::Schema.define(version: 20170704214222) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 20170704211629) do
     t.boolean  "confirmed",  default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.boolean  "read",            default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
