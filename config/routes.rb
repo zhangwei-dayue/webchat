c0ca0a1a3d0941ce3b00cac5fbe33af488d18afa
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-05 05:19:57 +0800

    增加聊天会话的controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "users/registrations" }
   resources :friendships
   resources :users, only: [:index]
+  resources :conversations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'friendships#index'
 end
