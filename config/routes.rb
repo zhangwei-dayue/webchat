f497c1e01f1006ac93ab7847aec3793f42e959fc
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-05 05:57:26 +0800

    完成第一版must have的部分，实现用户之间可以收发信息的功能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "users/registrations" }
   resources :friendships
   resources :users, only: [:index]
-  resources :conversations
+  resources :conversations do
+    resources :messages
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'friendships#index'
 end
