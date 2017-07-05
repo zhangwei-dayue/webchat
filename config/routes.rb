208fb86a6724a9e215d6541aa27367637101e3f6
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-05 21:09:11 +0800

    跟陌生人发送信息后双方自动互为好友

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "users/registrations" }
   resources :friendships
   resources :users, only: [:index]
-  resources :conversations do
-    resources :messages
+  resources :conversations, only: [:create] do
+    resources :messages do
+      collection do
+        post :view_all
+      end
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'friendships#index'
